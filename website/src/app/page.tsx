02a0326ee308118f81b1d34b2c3e46426b7be465
duyrk <99589013+duyrk@users.noreply.github.com>
2024-05-18 22:25:36 +0700

    feat: Add style to root page

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,9 +1,9 @@
 'use client';
-import { Box, Button, Input, Stack, Text } from '@mantine/core';
+import { Box, Button, Container, Input, Paper, Stack, Text } from '@mantine/core';
 import { v4 as uuidV4 } from 'uuid';
 import React from 'react';
 import { useRouter } from 'next/navigation';
-
+import classes from './_styles/index.module.css'
 export default function RootPage() {
   const router = useRouter();
   const [roomId, setRoomId] = React.useState('');
@@ -15,26 +15,33 @@ export default function RootPage() {
     if (roomId) {
       router.push(`/room/${roomId}`);
     } else {
-      alert("Please enter valid room id")
+      alert('Please enter valid room id');
     }
   };
   return (
-    <Box className="border-2">
-      <Stack align="center">
-        <Text className="text-3xl font-extrabold">Easy calling app</Text>
-        <Input
-          placeholder="Enter room id to join"
-          value={roomId}
-          onChange={(event) => {
-            setRoomId(event.target.value);
-          }}
-        />
-        <Button onClick={joinRoom}>Join a room</Button>
-      </Stack>
-      <Stack align="center" mt={20}>
-        <Text>--------------- OR ---------------</Text>
-        <Button onClick={createAndJoin}>Create a new room</Button>
-      </Stack>
-    </Box>
+    <Container size={420} pt={120}>
+      <Paper withBorder shadow="md" p={30} mt={30} radius="md">
+        <Stack>
+          <Text ta={'center'} className="text-3xl font-extrabold text-white">Thunder meeting ⚡</Text>
+          <Input
+            width={'100%'}
+            color='yellow'
+            placeholder="Enter room id to join"
+            value={roomId}
+            onChange={(event) => {
+              setRoomId(event.target.value);
+            }}
+            classNames={classes}
+          />
+          <Button fullWidth type="submit" variant="outline" color="yellow" onClick={joinRoom}>Join a room</Button>
+        </Stack>
+        <Stack align="center" mt={20}>
+          <Text>--------------- OR ---------------</Text>
+          <Button fullWidth type="submit" variant="outline" color="yellow"  onClick={createAndJoin}>
+            Create a new room
+          </Button>
+        </Stack>
+      </Paper>
+    </Container>
   );
 }
